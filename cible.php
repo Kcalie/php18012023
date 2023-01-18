ba06aaf022a86023f3c7f00573f565676a92dea2
KevinHaraux <kevin.haraux@gmail.com>
2023-01-18 09:43:10 +0100

    01

---
+++ cible.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bienvenue</title>
+</head>
+<body>
+    Bonjour !<br />
+    Votre nom est <?php echo $_GET['nom']; ?> , et votre prénom est <?php echo $_GET['prenom']; ?>. <br />
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenue</title>
+</head>
+<body>
+    <h2>des liens vers une page php</h2>
+    <a href="cible.php?nom=Léger&prenom=Gérald">Lien vers gérald</a>
+    <a href="cible.php?nom=Eclair&prenom=Buzz">Lien vers buzz</a>
+    <a href="cible.php?nom=Patate&prenom=Monsieur">Lien vers monsieur Patate</a>
+</body>
+</html>
